acd08ca3722e9ed5c22ffa2ae34a46ac887b3387
oshiojay <oshiobughieailakhu@gmail.com>
2026-06-18 18:06:05 +0100

    Remove vendorAuth from updatePackage route to streamline authentication

--- route/package.js
+++ route/package.js
@@ -6,7 +6,7 @@ const {authenticateToken, adminAuth, vendorAuth} = require('../middleware/auth')
 
 
 router.post('/:touristId', createPackageValidation, authenticateToken, vendorAuth, packageController.createPackage);
-router.put('/package/:id',updatePackageValidation, authenticateToken, vendorAuth,packageController.updatePackage);
+router.put('/package/:id',updatePackageValidation, authenticateToken, packageController.updatePackage);
 router.get('/all', authenticateToken, vendorAuth, packageController.getAllPackages);
 router.get('/:id', authenticateToken, vendorAuth, packageController.getPackageById);
 router.delete('/:id', authenticateToken, vendorAuth, packageController.deletePackage);
