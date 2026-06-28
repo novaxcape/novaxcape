25ae68d21010dd262685633c7713cf8bdf28e24a
oshiojay <oshiobughieailakhu@gmail.com>
2026-06-28 23:50:25 +0100

    Refactor vendor migration, update favourite model association, and adjust tourist model relationship

--- models/favourite.js
+++ models/favourite.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      favourite.hasMany(models.Tourist, {
+      favourite.belongsTo(models.Tourist, {
         foreignKey: 'centreId',
         as: 'tourist'
       });
